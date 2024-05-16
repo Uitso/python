85ceeb6de42aee2893463e9ad7824dd3ebd0f219
Uitso <vladboevec@gmail.com>
2024-05-16 22:13:47 +0300

    Colorama

---
+++ hw.5.py
@@ -0,0 +1,5 @@
+from colorama import *
+
+
+print(f'{Fore.RED + Style.BRIGHT}Я{Style.NORMAL} думаю что самые важные функции в модуле {Fore.RED + Style.BRIGHT + Back.BLACK}'
+      f'Colorama{Style.NORMAL + Back.RESET} это изменение вида текста а также фона в консоли')
